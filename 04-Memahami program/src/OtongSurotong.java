a8f9d04504c0b70f41b366f2ed94828e73009b3c
Harpan20 <regytaharpan20102001@gmail.com>
2019-12-12 12:37:15 +0700

    java-per-13

---
+++ 04-Memahami program/src/OtongSurotong.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author REGYTA HARPAN
+ * NAMA     : REGYTA HARPAN
+ * KELAS    : TI-B
+ * NIM      : A2.1900145
+ */
+public class OtongSurotong {
+    public static void main(String[] args){
+        System.out.print("Nama ku adalah Otong, teman si ucup");
+    }
+}
---
+++ 05-Organisasi Program dengan Package/src/com/ucup/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ucup;
+
+/**
+ *
+ * @author REGYTA HARPAN
+ * NAMA     : REGYTA HARPAN
+ * KELAS    : TI-B
+ * NIM      : A2.1900145
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
